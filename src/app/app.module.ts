9d6e16554e87773d4e4a5b8af109fc1b52c1b5c6
Pierre Rochard <pierre@rochard.org>
2017-02-09 10:25:53 -0500

    bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import {RestSchemaService, RestClient, APP_CONFIG, AuthenticationService} from "angular2-postgrest/dist";
+import {RestSchemaService, RestClient, APP_CONFIG, AuthenticationService} from "angular2-postgrest";
 
 import { AppComponent } from './app.component';
 import {SchemaGuard} from "./guards/schema.guard";
 import {routing} from "./app.routing";
 import {HomeComponent} from "./pages/home/home.component";
-import {AppConfig} from "./pages/app.config";
+import {AppConfig} from "./app.config";
 import {MenubarComponent} from "./menubar/menubar.component";
 import {MenubarModule} from "primeng/components/menubar/menubar";
 import {MenuService} from "./services/menu.service";
