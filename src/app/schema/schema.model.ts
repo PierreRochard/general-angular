116ef1798f2dfd298798edabfe1b289f22b71bd8
Pierre Rochard <pierre@rochard.org>
2017-02-20 20:00:54 -0500

    WIP

---
+++ src/app/schema/schema.model.ts
@@ -0,0 +1,40 @@
+export interface Property {
+  format: string,
+  type: string,
+}
+
+export interface Definition {
+  required?: string[];
+  properties: { [name: string]: Property[] };
+  type:  string;
+}
+
+export interface Path {
+  get?: Endpoint;
+  post?: Endpoint;
+  delete?: Endpoint;
+  patch?: Endpoint;
+}
+
+export interface Endpoint {
+  tags: string[];
+  produces: string[];
+  consumes?: string[];
+  parameters: Parameter[];
+  responses: { [code: number]: Response}
+
+}
+
+export interface Parameter {
+  required: boolean;
+  in: string;
+  name: string;
+  type: string;
+  description: string;
+  enum?: string[];
+  schema?: {$ref: string};
+}
+
+export interface Response {
+  description: string;
+}
--- src/app/schema/schema.reducers.ts
+++ src/app/schema/schema.reducers.ts
@@ -1,23 +1,30 @@
+import { createSelector } from 'reselect';
+
 import * as schema from './schema.actions';
 
+import {Definition, Path} from "./schema.model";
+import {create} from "domain";
+
+
 export interface State {
-  schema: any;
-  isFetching: boolean,
-  valid_status: boolean,
-  lastUpdated?: Date,
+  paths: { [name: string]: Path };
+  definitions: { [name: string]: Definition };
+  isValid: boolean;
+  lastUpdated?: Date;
+  selectedPathName?: string | null;
 }
 
 const initialState: State = {
-  schema: {'definitions': {}},
-  valid_status: false,
-  isFetching: false,
+  paths: {},
+  definitions: {},
+  isValid: false,
 };
 
 export function reducer(state = initialState, action: schema.Actions): State {
   switch (action.type) {
     case schema.ActionTypes.INVALIDATE_SCHEMA: {
       return Object.assign({}, state, {
-        valid_status: false,
+        isValid: false,
       });
     }
     case schema.ActionTypes.REQUEST_SCHEMA: {
@@ -28,7 +35,7 @@ export function reducer(state = initialState, action: schema.Actions): State {
     case schema.ActionTypes.RECEIVE_SCHEMA: {
       return Object.assign({}, state, {
         isFetching: false,
-        valid_status: true,
+        isValid: true,
         schema: action.payload,
         lastUpdated: Date.now(),
       })
@@ -39,5 +46,19 @@ export function reducer(state = initialState, action: schema.Actions): State {
   }
 }
 
-export const getSchema = (state: State) => state.schema;
-export const getStatus = (state: State) => state.valid_status;
+export const getPaths = (state: State) => state.paths;
+export const getDefinitions = (state: State) => state.definitions;
+export const getStatus = (state: State) => state.isValid;
+export const getLastUpdated = (state: State) => state.lastUpdated;
+export const getSelectedPathName = (state: State) => state.selectedPathName;
+
+export const getPathNames = (state: State) => Object.keys(state.paths);
+export const getDefinitionNames = (state: State) => Object.keys(state.definitions);
+
+export const getSelectedPath = createSelector(getPaths, getSelectedPathName, (paths, selectedPathName) => {
+  return paths[selectedPathName];
+});
+export const getSelectedPathPostBodyDefinition = createSelector(getSelectedPath, getDefinitions, (selectedPath, definitions) => {
+  let definition_name = selectedPath.post.parameters.filter(parameter => parameter.name === 'args')[0].schema.$ref.split('/')[-1];
+  return definitions[definition_name];
+});
