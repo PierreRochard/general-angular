37aa74a67ad2bc1f269d25d82b940c6727a14523
Pierre Rochard <pierre@rochard.org>
2017-04-02 11:09:53 -0400

    debug

--- src/app/table/table-datatable.component.ts
+++ src/app/table/table-datatable.component.ts
@@ -6,23 +6,37 @@ import {Component, Input, OnChanges, EventEmitter, Output, SimpleChanges} from '
              [value]="records"
              [rows]="10"
              [paginator]="true"
+             sortMode="multiple"
+             (onRowSelect)="onRowSelect($event)" 
+             (onRowUnselect)="onRowUnselect($event)"
               >
+              <p-column [style]="{'width':'38px'}" selectionMode="multiple"></p-column>
                 <p-column *ngFor="let columnName of columnNames" 
                           [field]="columnName" 
                           [header]="columnName"
                           [style]="{'width':'100%'}"
                           [sortable]="true"
                 ></p-column>
-              </p-dataTable>`
+              </p-dataTable>
+{{selectedRecords | json}}`
 })
 export class TableDatatableComponent implements OnChanges {
   public columnNames: string[];
+  public selectedRecords: any[];
 
   @Input() records:any[];
 
-  ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges() {
     this.columnNames = (this.records === null || this.records.length === 0) ?
       [] :
       Object.keys(this.records[0]);
+  }
+
+  onRowSelect(event) {
+    this.selectedRecords = event.data;
+  }
+
+  onRowUnselect(event) {
+    this.selectedRecords = event.data;
   }
 }
