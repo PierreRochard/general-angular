639c3eed792d619be75c4e38d9cbd69f51c4e4da
Pierre Rochard <pierre@rochard.org>
2017-05-02 22:02:20 -0400

    experimenting

--- src/app/table/table-datatable.component.ts
+++ src/app/table/table-datatable.component.ts
@@ -5,16 +5,19 @@ import {Component, Input, OnChanges, EventEmitter, Output, SimpleChanges} from '
   template: `<p-dataTable
              [dataKey]="dataKey"
              [paginator]="paginator"
-             [value]="records"
+             (onColReorder)="onColReorder($event)"
+             (onColumnResize)="onColumnResize($event)"
+             [reorderableColumns]="reorderableColumns"
+             [resizableColumns]="resizableColumns"
              [rows]="rows"
+             (selectionChange)="selectionChange($event)"
              [sortMode]="sortMode"
-             (selectionChange)="selectionChange($event)" 
+             [value]="records"
               >
-              <p-column [style]="{'width':'38px'}" selectionMode="multiple"></p-column>
+              <p-column [selectionMode]="selectionMode"></p-column>
                 <p-column *ngFor="let columnName of columnNames" 
                           [field]="columnName" 
                           [header]="columnName"
-                          [style]="{'width':'100%'}"
                           [sortable]="true"
                 ></p-column>
               </p-dataTable>`
@@ -23,8 +26,11 @@ export class TableDatatableComponent implements OnChanges {
   public columnNames: string[];
   public dataKey: string = 'id';
   public paginator: boolean = true;
+  public reorderableColumns: boolean = true;
+  public resizableColumns: boolean = true;
   public rows: number = 10;
   public selectedRecords: any[] = [];
+  public selectionMode: string = 'multiple';
   public sortMode: string = 'multiple';
 
   @Input() records:any[];
@@ -37,6 +43,14 @@ export class TableDatatableComponent implements OnChanges {
 
   selectionChange(event) {
     this.selectedRecords = event;
+  }
+
+  onColReorder(event) {
+    console.log(event);
+  }
+
+  onColumnResize(event) {
+    console.log(event);
   }
 
 }
